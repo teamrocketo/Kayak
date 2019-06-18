7a3c6da20c1c3a5908256292e2c15d02eb8865e4
Jony635 <jony635molina@gmail.com>
2019-06-18 11:18:28 +0200

    Basic input configured

---
+++ RocketoGame/Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rowForce = 10f;
+
+    private bool rightTriggerPressed = false;
+    private bool leftTriggerPressed = false;
+
+    void Update()
+    {
+        InputManagement();
+        
+
+    }
+
+    void InputManagement()
+    {
+        if (Input.GetAxis("RightTrigger") > 0)
+        {
+            if (!rightTriggerPressed && !leftTriggerPressed)
+            {
+                OnRightTriggerPressed();
+            }
+
+            rightTriggerPressed = true;
+        }
+        else
+        {
+            rightTriggerPressed = false;
+        }
+
+        if (Input.GetAxis("LeftTrigger") > 0)
+        {
+            if (!leftTriggerPressed && !rightTriggerPressed)
+            {
+                OnLeftTriggerPressed();
+            }
+            leftTriggerPressed = true;
+        }
+        else
+        {
+            leftTriggerPressed = false;
+        }
+    }
+
+    void OnRightTriggerPressed()
+    {
+        //TODO: APPLY FORCES AND SYNCRONIZE WITH THE ANIMS
+    }
+
+    void OnLeftTriggerPressed()
+    {
+        //TODO: APPLY FORCES AND SYNCRONIZE WITH THE ANIMS
+    }
+}
