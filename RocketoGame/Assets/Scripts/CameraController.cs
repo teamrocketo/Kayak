ea7d0a7b6b665d47b81d9872d288e5806c5f632e
Guillermo García Subirana <guillermogarsub@gmail.com>
2019-06-18 13:59:08 +0200

    merge for god

--- RocketoGame/Assets/Scripts/CameraController.cs
+++ RocketoGame/Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+﻿
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,7 @@ public class CameraController : MonoBehaviour
     public Transform player2;
 
     public float smooth_speed = 0.125f;
-    public float max_distance_between_players = 10.0f;
+    public float max_distance_between_players = 20.0f;
     public Vector3 offset;
 
     public Transform current_winner;
@@ -18,9 +19,14 @@ public class CameraController : MonoBehaviour
     public float distance_between_players;
     public Vector3 v_winner;
 
+    [Range(0.0f, 1.0f)]
+    public float offset_multipler = 0.33f;
+
     // Start is called before the first frame update
     void Start()
     {
+        offset.Set(0.0f, this.transform.position.y, this.transform.position.z);
+
         current_winner = player1;
         current_loser = player2;
     }
@@ -41,19 +47,24 @@ public class CameraController : MonoBehaviour
         // Calculate the 1/3 distance between the 1st and 2nd place
         distance_between_players = Vector3.Distance(player1.position, player2.position);
 
-
         if (max_distance_between_players < distance_between_players)
             distance_between_players = max_distance_between_players;
 
-        Debug.Log(distance_between_players);
+        if (distance_between_players < 10.0f)
+        {
+            offset_multipler = 0.5f;
+        }
+        else if (distance_between_players >= 10.0f)
+            offset_multipler = 0.33f;
 
         v_winner = current_loser.position - current_winner.position;
         v_winner.Normalize();
-        v_winner = current_winner.position + (v_winner * (0.33f * distance_between_players));
+        v_winner = current_winner.position + (v_winner * (offset_multipler * distance_between_players));
 
         // Move the camera
         Vector3 desired_position = v_winner + offset;
         Vector3 smoothed_position = Vector3.Lerp(transform.position, desired_position, smooth_speed);
+
         transform.position = smoothed_position;
 	}
 
