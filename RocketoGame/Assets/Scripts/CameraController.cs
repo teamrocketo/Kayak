ce6c1d192e74e1eedadda0196322eee38c3ae99b
Guillermo García Subirana <guillermogarsub@gmail.com>
2019-06-18 12:24:55 +0200

    Update CameraController.cs

--- RocketoGame/Assets/Scripts/CameraController.cs
+++ RocketoGame/Assets/Scripts/CameraController.cs
@@ -11,33 +11,33 @@ public class CameraController : MonoBehaviour
     public float smooth_speed = 0.125f;
     public Vector3 offset;
 
-    private Transform current_target;
+    private Transform current_winner;
 
     // Start is called before the first frame update
     void Start()
     {
-        current_target = player1;
+        current_winner = player1;
     }
 
 	private void Update()
 	{
         if(Input.GetKeyDown("space")){
-            current_target = player2;
+            current_winner = player2;
         }
 
         if (Input.GetKeyDown("up"))
         {
-            current_target = player1;
+            current_winner = player1;
         }
 
 	}
 
 	void FixedUpdate()
     {
-        Vector3 desired_position = current_target.position + offset;
+        Vector3 desired_position = current_winner.position + offset;
         Vector3 smoothed_position = Vector3.Lerp(transform.position, desired_position, smooth_speed);
         transform.position = smoothed_position;
 
-        transform.LookAt(current_target);
+        //transform.LookAt(current_target);
     }
 }
