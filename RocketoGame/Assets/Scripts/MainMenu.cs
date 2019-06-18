d07f716d99979a3f68cec0587a0c8dfb75e320dc
Guillem Costa Miquel <guillemcm3@TR12.CAT>
2019-06-18 22:41:29 +0200

    menu cinemachine

---
+++ RocketoGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class MainMenu : MonoBehaviour
+{
+    public CinemachineVirtualCamera virtualCameraPrefab;
+    public Transform credits;
+    public Transform menu;
+    bool startBlend = false;
+    public float _ConstantForce = 1.0f;
+    CinemachineTrackedDolly dolly;
+
+    enum currentMenu { menu, goToCredits, credits, goToMenu };
+    currentMenu state = currentMenu.menu;
+    void Start()
+    {
+        dolly = virtualCameraPrefab.GetCinemachineComponent<CinemachineTrackedDolly>();
+    }
+    
+    void Update()
+    {
+        switch(state)
+        {
+            case currentMenu.menu:
+                if (Input.GetKeyDown(KeyCode.Space))
+                    state = currentMenu.goToCredits;
+                break;
+            case currentMenu.goToCredits:
+                dolly.m_PathPosition += Time.deltaTime * _ConstantForce;
+                if (dolly.m_PathPosition >= 2)
+                {
+                   // virtualCameraPrefab.LookAt = credits;
+                    //virtualCameraPrefab.m_LookAt = credits;
+                }
+                if (dolly.m_PathPosition >= 4)
+                {
+                 
+                    dolly.m_PathPosition = 4;
+                    state = currentMenu.credits;
+                }
+                break;
+            case currentMenu.credits:
+                if (Input.GetKeyDown(KeyCode.Space))
+                    state = currentMenu.goToMenu;
+                break;
+            case currentMenu.goToMenu:
+                dolly.m_PathPosition -= Time.deltaTime * _ConstantForce;
+                if (dolly.m_PathPosition <= 2)
+                {
+                  //  virtualCameraPrefab.LookAt =
+                   // virtualCameraPrefab.m_LookAt = menu;
+                }
+                if (dolly.m_PathPosition <= 0)
+                {
+                    
+                    dolly.m_PathPosition = 0;
+                    state = currentMenu.menu;
+                }
+                break;
+        }
+    }
+}
