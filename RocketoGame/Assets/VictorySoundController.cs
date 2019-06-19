fd0ea68c9251fcaafa976d8985432b3e37df9baf
nintervik <vmasogarcia@gmail.com>
2019-06-19 10:40:31 +0200

    trumpeet!

---
+++ RocketoGame/Assets/VictorySoundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictorySoundController : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip audioClip;
+    public CameraController cameraController;
+    private bool played = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraController.game_end && !played)
+        {
+            audioSource.PlayOneShot(audioClip);
+            played = true;
+        }
+    }
+}
