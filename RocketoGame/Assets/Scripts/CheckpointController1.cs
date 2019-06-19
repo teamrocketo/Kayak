d06e46cc3b89831480cd71c29520d86c5181fcfd
Guillem Costa Miquel <guillemcm3@TR12.CAT>
2019-06-19 05:18:53 +0200

    end game

---
+++ RocketoGame/Assets/Scripts/CheckpointController1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController1 : MonoBehaviour
+{
+    public CameraController camera_script;
+    public bool set_first = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(set_first){
+            set_first = false;
+            camera_script.SetFirstPlayer(other.gameObject);
+            camera_script.game_end = true;
+        }
+    }
+}
